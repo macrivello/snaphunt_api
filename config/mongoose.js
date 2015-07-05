4cf91e71e2308d3a5790b75618ffe68c8cb75ee8
Michael Crivello <michaelacrivello@gmail.com>
2015-07-05 14:50:54 -0700

    changing resetDB to call remove() on all models instead of drop collection

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,12 +5,20 @@ module.exports = function() {
 	var mongooseDb = mongoose.connect(config.dbURI);
 
     // Load Models
-    require('../app/models/userdigest.server.model');
-    require('../app/models/theme.server.model');
-    require('../app/models/photo.server.model');
-    require('../app/models/round.server.model');
-    require('../app/models/user.server.model');
-    require('../app/models/game.server.model');
+    var UserDigest = require('../app/models/userdigest.server.model'),
+    Theme = require('../app/models/theme.server.model'),
+    Photo = require('../app/models/photo.server.model'),
+    Round = require('../app/models/round.server.model'),
+    User = require('../app/models/user.server.model'),
+    Game = require('../app/models/game.server.model');
+
+    var models = [require('mongoose').model('User'),
+                require('mongoose').model('UserDigest'),
+                require('mongoose').model('Game'),
+                require('mongoose').model('Round'),
+                require('mongoose').model('Photo'),
+                require('mongoose').model('Theme')];
+
 
     // TODO: There is a user and pass field on mongoose db connections. use this for admin calls?
     // TODO: This needs some refactoring.
@@ -33,16 +41,29 @@ module.exports = function() {
         if (db) {
             console.log("Deleting everything in DB: " + dbName);
 
-            for (var key in db.collections) {
-                console.log("dropping collection: " + key);
-                mongoose.connection.db.dropCollection(key, function(err, result) {
-                    if (err) {
-                        console.log("Error: " + err);
-                    } else {
-                        console.log("dropped collection. " + result);
-                    }
-                });
+            //for (var key in db.collections) {
+            //    console.log("dropping collection: " + key);
+            //    mongoose.connection.db.dropCollection(key, function(err, result) {
+            //        if (err) {
+            //            console.log("Error: " + err);
+            //        } else {
+            //            console.log("dropped collection. " + result);
+            //        }
+            //    });
+            //}
+
+            for (var i = 0; i < models.length; i++) {
+                var model = models[i];
+                console.log("Removing all : " + model.modelName);
+                model.remove({})
+                    .then(function(_model) {
+                        console.log("Removed all instances of " + _model);
+                    }).catch(function(err) {
+                        console.log("Error removing " + model + ". " + err);
+                    });
             }
+
+
             return true;
         }
         return false;
